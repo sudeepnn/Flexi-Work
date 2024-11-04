1910d937d18ca4ea762909c9eff4872f4f55630d
sudeepnn <sudeep2002naik@gmail.com>
2024-11-04 09:41:29 +0530

    central user db

--- users/src/config/database.ts
+++ users/src/config/database.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/users');
+    await mongoose.connect('mongodb+srv://sudeep2002naik:Hello123@cluster0.ct290.mongodb.net/users');
     console.log('MongoDB connected')
   } catch (error) {
     console.error(error);
